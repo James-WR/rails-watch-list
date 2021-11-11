21104c2e965ffe2f25421bbd2afca4bbaae407e3
James Robbs <jamesrobbs01@gmail.com>
2021-11-11 15:29:37 +0000

    database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+Movie.destroy_all
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_serialized = URI.open(url).read
+movies_json = JSON.parse(movie_serialized)
+movies = movies_json["results"]
+movies.each do |movie|
+  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: movie['poster_path'], rating: movie['vote_average'])
+  puts "Movie called #{movie['title']} created."
+end
