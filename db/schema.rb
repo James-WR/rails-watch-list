536e457369a88e5ebdaa89fc8d98c7204f6eafb3
James Robbs <jamesrobbs01@gmail.com>
2021-11-11 14:27:31 +0000

    updated and migrated models

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,12 @@ ActiveRecord::Schema.define(version: 2021_11_11_121940) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.integer "list_id"
-    t.integer "movie_id"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -38,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_11_11_121940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
