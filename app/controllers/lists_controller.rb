0bc4b035a73e0145742e994ed31ac4c4ce031331
James Robbs <jamesrobbs01@gmail.com>
2021-11-12 12:18:24 +0000

    create lists, page linking and redirects

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,5 +6,24 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :uniqueness)
+  end
+
+
 end
